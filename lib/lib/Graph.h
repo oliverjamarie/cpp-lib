454e6beaae585ae9cef4bd304a3dd5970756b459
Oliver Marie <oliverjamarie@gmail.com>
2022-02-01 17:01:31 +0100

    Generic Graph Implementation

---
+++ lib/lib/Graph.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define TEMPLATE template <class T>
+
+#include "ArrayList.h"
+
+TEMPLATE
+class Graph {
+	ArrayList<GraphNode> nodes;
+	GraphNode root;
+
+
+};
+
+TEMPLATE
+class GraphNode {
+
+};
+
+
